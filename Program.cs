cab6c1e439f81eb9c838cad2730a5d85b03d76b4
Jason Warren <contact.jwarren@gmail.com>
2024-11-06 17:03:48 +0000

    Classist Permissions Manager

--- Program.cs
+++ Program.cs
@@ -1,37 +1,116 @@
-﻿// => Setup
-string result;
-string symbol;
+﻿// => Inputs
+int level = new Random().Next(0, 101);
+int roles = new Random().Next(0, 4);
 
-List<string> results = new List<string>();
-List<string> symbols = new List<string>();
+string permission = "";
+if (roles > 0)
+{
+  if (roles == 1)
+    permission = "Admin";
+  else if (roles == 2)
+    permission = "Manager";
+  else if (roles >= 3)
+    permission = "Admin|Manager";
+}
 
-int games = results.Count;
+Console.WriteLine($"Level: {level}");
+Console.WriteLine($"Permissions: {permission}");
 
-// => Loop
-// ==> Calculations
-int flip = new Random().Next(0, 2);
 
-result = (flip == 0) ? "Heads" : "Tails";
-symbol = (flip == 0) ? "Ⓗ" : "Ⓣ";
+// => Setup
+bool isAdmin = permission.Contains("Admin");
+bool isManager = permission.Contains("Manager");
 
-results.Add(result);
-symbols.Add(symbol);
+bool hasAccess = isAdmin ? true : false;
+bool couldAccess = (permission != "" && level >= 20) ? true : false;
 
-games = results.Count;
 
-// ==> Output
-// ===> Typography
-string game = games < 10 ? $"0{games}" : $"{games}";
+// => Assign Tags
+string userHigh;
 
-// ===> This Game
-Console.WriteLine($"  Coin Toss №{game}  ");
-Console.WriteLine($"=================");
-Console.WriteLine($"‖  {symbol}  |  {result}  ‖");
-Console.WriteLine($"=================");
+if (isAdmin)
+{
+  userHigh = level > 55 ? "Super Admin" : "Admin";
+}
+else if (isManager)
+{
+  userHigh = "Manager";
+  hasAccess = false;
+  couldAccess = level >= 20 ? true : false;
+}
+else
+{
+  userHigh = "Peasant";
+  hasAccess = false;
+  couldAccess = false;
+}
 
-// ===> All Games
-Console.WriteLine("All Results");
-results.ForEach(result => Console.Write($"{result} "));
-Console.WriteLine();
-symbols.ForEach(symbol => Console.Write($"{symbol} "));
-Console.WriteLine();
+// => String Builders
+string line2;
+
+// ==> Line 1: Salutation Builder
+string line1;
+string salute;
+string punct = ".";
+
+if (userHigh == "Super Admin")
+{
+  salute = "Hail";
+  punct = "!";
+}
+else if (userHigh == "Admin")
+{
+  salute = "Welcome";
+}
+else if (userHigh == "Manager")
+{
+  salute = "Hello";
+}
+else
+{
+  salute = "Yes";
+  punct = "?";
+}
+
+line1 = $"{salute}, {userHigh}{punct}";
+
+// ==> Line 2: Statement Builder
+if (userHigh == "Super Admin")
+{
+  line2 = "Let me know if I may be of assistance to you.";
+}
+else if (hasAccess)
+{
+  line2 = "";
+}
+else if (couldAccess)
+{
+  line2 = "Contact an Admin for access.";
+}
+else 
+{
+  line2 = "You do not have sufficient privileges.";
+}
+
+
+// => Output
+Console.WriteLine($"{line1}");
+if (line2 != "")
+  Console.WriteLine($"{line2}");
+
+/* ====== Goal ======
+  Admin with a level greater than 55
+    Welcome, Super Admin user.
+
+  Admin with a level less than or equal to 55
+    Welcome, Admin user.
+
+  Manager with a level 20 or greater
+    Contact an Admin for access.
+
+  Manager with a level less than 20
+    You do not have sufficient privileges.
+
+  Not an Admin or a Manager
+    You do not have sufficient privileges.
+*/
