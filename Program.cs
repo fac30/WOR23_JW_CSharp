f874b9b0b4b2f5ae6d6d637fa477a6922d2d4fe8
Jason Warren <contact.jwarren@gmail.com>
2024-11-07 12:10:57 +0000

    Product Code Parser

--- Program.cs
+++ Program.cs
@@ -1,116 +1,56 @@
-﻿// => Inputs
-int level = new Random().Next(0, 101);
-int roles = new Random().Next(0, 4);
+﻿// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
 
-string permission = "";
-if (roles > 0)
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
 {
-  if (roles == 1)
-    permission = "Admin";
-  else if (roles == 2)
-    permission = "Manager";
-  else if (roles >= 3)
-    permission = "Admin|Manager";
+  case "01":
+    type = "Sweat shirt";
+    break;
+  case "02":
+    type = "T-Shirt";
+    break;
+  case "03":
+    type = "Sweat pants";
+    break;
+  default:
+    type = "Other";
+    break;
 }
 
-Console.WriteLine($"Level: {level}");
-Console.WriteLine($"Permissions: {permission}");
-
-
-// => Setup
-bool isAdmin = permission.Contains("Admin");
-bool isManager = permission.Contains("Manager");
-
-bool hasAccess = isAdmin ? true : false;
-bool couldAccess = (permission != "" && level >= 20) ? true : false;
-
-
-// => Assign Tags
-string userHigh;
-
-if (isAdmin)
+switch (product[1])
 {
-  userHigh = level > 55 ? "Super Admin" : "Admin";
-}
-else if (isManager)
-{
-  userHigh = "Manager";
-  hasAccess = false;
-  couldAccess = level >= 20 ? true : false;
-}
-else
-{
-  userHigh = "Peasant";
-  hasAccess = false;
-  couldAccess = false;
+  case "BL":
+    color = "Black";
+    break;
+  case "MN":
+    color = "Maroon";
+    break;
+  default:
+    color = "White";
+    break;
 }
 
-// => String Builders
-string line2;
-
-// ==> Line 1: Salutation Builder
-string line1;
-string salute;
-string punct = ".";
-
-if (userHigh == "Super Admin")
+switch (product[2])
 {
-  salute = "Hail";
-  punct = "!";
-}
-else if (userHigh == "Admin")
-{
-  salute = "Welcome";
-}
-else if (userHigh == "Manager")
-{
-  salute = "Hello";
-}
-else
-{
-  salute = "Yes";
-  punct = "?";
+  case "S":
+    size = "Small";
+    break;
+  case "M":
+    size = "Medium";
+    break;
+  case "L":
+    size = "Large";
+    break;
+  default:
+    size = "One Size Fits All";
+    break;
 }
 
-line1 = $"{salute}, {userHigh}{punct}";
-
-// ==> Line 2: Statement Builder
-if (userHigh == "Super Admin")
-{
-  line2 = "Let me know if I may be of assistance to you.";
-}
-else if (hasAccess)
-{
-  line2 = "";
-}
-else if (couldAccess)
-{
-  line2 = "Contact an Admin for access.";
-}
-else 
-{
-  line2 = "You do not have sufficient privileges.";
-}
-
-
-// => Output
-Console.WriteLine($"{line1}");
-if (line2 != "")
-  Console.WriteLine($"{line2}");
-
-/* ====== Goal ======
-  Admin with a level greater than 55
-    Welcome, Super Admin user.
-
-  Admin with a level less than or equal to 55
-    Welcome, Admin user.
-
-  Manager with a level 20 or greater
-    Contact an Admin for access.
-
-  Manager with a level less than 20
-    You do not have sufficient privileges.
-
-  Not an Admin or a Manager
-    You do not have sufficient privileges.
-*/
+Console.WriteLine($"Product: {size} {color} {type}");
