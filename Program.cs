f3d44e47abeb6fb2ab3d6402a6914bb460872636
Jason Warren <contact.jwarren@gmail.com>
2024-11-07 19:00:44 +0000

    RPG v2 - repeatedly beat a monster until it's dead - make no choices at any point

--- Program.cs
+++ Program.cs
@@ -1,31 +1,38 @@
-﻿// => Dev Tools
-bool debug = true;
+﻿bool debug = true;
 
-// => Game Setup
+// => Setup
+Random die = new Random();
+
+// ==> Entities
+// ===> Hero
+int heroRawLevel = 0;
+int heroRawHealth = 10;
+int heroRawBonus = 0;
+int heroRawArmor = 0;
+string heroStatBonus = "";
+string heroStatArmor = "";
+int dMinHero = 1;
+int dMaxHero = 11;
+
+// ===> NPC
+int npcRawLevel = 0;
+int npcRawHealth = 10;
+int npcRawBonus = 0;
+int npcRawArmor = 0;
+int dMinNpc = 1;
+int dMaxNpc = 11;
+
+// ==> Structure
 string phase1 = "start";
 string phase2 = "";
-
-Random heroDice = new Random();
-Random monsterDice = new Random();
-
-int heroHealth = 10;
-int heroBonus = 0;
-int heroArmor = 0;
-int heroLevel = 1;
-
-int monsterHealth = 0;
-int monsterBonus = 0;
-int monsterArmor = 0;
-
+string phase3 = "";
 int battleRound = 0;
 int nullRounds = 0;
-
 bool gameRunning = true;
 
 while (gameRunning)
 {
-  // => Phase: Game Start
-  while (phase1 == "start")
+  while (phase1 == "start") //=> Game Start
   {
     Console.Clear();
     
@@ -41,116 +48,179 @@ while (gameRunning)
     phase1 = "";
   }
 
-  // => Phase: Game End
-  while (phase1 == "gameEnd")
+  while (phase1 == "gameEnd") //=> Game End
   {
     Console.WriteLine($"Game Over");
     Console.WriteLine($"Press Key to End");
+
     Console.ReadLine();
 
     gameRunning = false;
+    phase1 = "";
   }
   
-  // => Phase: Battle
-  while (phase1 == "battle")
+  while (phase1 == "battle") //=> Battle
   {
-    while (phase2 == "")
+    while (phase2 == "") //==> Battle Setup
     {
       battleRound = 0;
       nullRounds = 0;
       
-      monsterHealth = 10;
-      monsterBonus = 0;
-      monsterArmor = 0;
+      // todo Implement logic for varying enemy stats
+      npcRawLevel = heroRawLevel;
+      npcRawHealth = 10 + npcRawLevel;
 
-      // ==> Battle Information
       Console.WriteLine($"======================");
+      Console.WriteLine($"A level {npcRawLevel} monster appears!");
       Console.WriteLine($"Press key to start battle");
-      Console.ReadLine();
 
+      Console.ReadLine();
+      Console.Clear();
+      
       phase2 = "loop";
     }
 
-    while (phase2 == "loop")
+    while (phase2 == "loop") //==> Battle Loop
     {
+      phase3 = "";
+
       do
       {
-        int shift = 0;
-        
-        // ===> Start Round
         battleRound++;
-        Console.WriteLine($"Press key to start Round {battleRound}");
-        Console.ReadLine();
+        int hpShift = 0;
 
-        // ===> Hero Turn
-        Console.WriteLine($"======================");
-        Console.WriteLine($"Hero: Turn {battleRound}");
-        Console.WriteLine($"Level {heroLevel} • {heroHealth} HP • Block {heroArmor} damage • +{heroBonus} to rolls");
-        Console.WriteLine($"----------------------");
-        Console.WriteLine($"Press key to attack");
-        Console.ReadLine();
-
-        int heroRoll = 0;
-        shift += heroRoll;
-
-        // ===> Monster Turn
-        Console.WriteLine($"======================");
-        Console.WriteLine($"Monster: Turn {battleRound}");
-        Console.WriteLine($"{monsterHealth} HP • Block {monsterArmor} damage • +{monsterBonus} to rolls");
-        Console.WriteLine($"----------------------");
-        Console.WriteLine($"Press key to defend");
-        Console.ReadLine();
-
-        int monsterRoll = 0;
-        shift += monsterRoll;
-
-        // ===> Evaluate
-        if (shift == 0)
+        while (phase3 == "") //===> Round Start
         {
-          nullRounds++;
-          Console.WriteLine($"Null Rounds: {nullRounds}.");
+          Console.WriteLine($"======================");
+
+          Console.WriteLine($"Press key to start Round {battleRound}");
+          Console.ReadLine();
+
+          phase3 = "heroTurn";
+        }
+
+        while (phase3 == "heroTurn") //===> Hero's Turn
+        {
+          Console.WriteLine($"----------------------");
+          Console.WriteLine($"Hero's Turn");
+          Console.WriteLine($"Level {heroRawLevel} • {heroRawHealth} HP {heroStatArmor} {heroStatBonus}");
+          Console.WriteLine("");
+          Console.WriteLine($"Press key to attack");
+
+          Console.ReadLine();
+
+          int roll = die.Next(dMinHero, dMaxHero) + heroRawBonus;
+          int attack = roll + heroRawBonus;
+          int damage = attack - npcRawArmor;
+          npcRawHealth -= damage;
+
+          hpShift += attack;
+
+          Console.WriteLine($"You rolled {roll}");
+          if (attack != roll)
+            Console.WriteLine($"Your skill of {heroRawBonus} boosts that to {attack}");
+          if (damage != attack)
+            Console.WriteLine($"The enemy blocked {npcRawArmor}");
+          Console.WriteLine($"The enemy loses {damage} HP, leaving it with {(npcRawHealth > 0 ? npcRawHealth : 0)} HP");
+
+          Console.Write("Continue?");
+          Console.ReadLine();
+
+          phase3 = "heroCheck";
+        }
+
+        while (phase3 == "heroCheck") //===> Check HP
+        {
+          if (heroRawHealth > 0 && npcRawHealth > 0)
+            phase3 = "npcTurn";
+          else
+            phase2 = "result";
+            phase3 = "cleanup";
+        }
+
+        while (phase3 == "npcTurn") //===> NPC's Turn
+        {
+          Console.WriteLine($"----------------------");
+          Console.WriteLine($"Enemy'S Turn");
+          Console.WriteLine($"{npcRawHealth} HP");
+          Console.WriteLine("");
+          Console.WriteLine($"Press key to defend");
+          
+          Console.ReadLine();
+
+          int attack = die.Next(dMinHero, dMaxHero);
+          hpShift += attack;
+
+          phase3 = "check";
+        }
+
+        while (phase3 == "npcCheck") //===> Check HP
+        {
+          if (heroRawHealth > 0 && npcRawHealth > 0)
+            phase3 = "nullCheck";
+          else
+            phase2 = "result";
+            phase3 = "";
+        }
+
+        while (phase3 == "nullCheck") //===> Check for Dull Combat
+        {
+          Console.WriteLine($"Shift this round: {hpShift}");
+          if (hpShift == 0) { nullRounds++;}
+          {
+            Console.WriteLine($"Null Rounds: {nullRounds}.");
+          }
+
+          phase3 = "cleanup";
+        }
+
+        while (phase3 == "cleanup") //===> Cleanup Round
+        {
           Console.WriteLine($"Continue?");
           Console.ReadLine();
-          Console.Clear();
+
+          phase3 = "heroTurn";
         }
-      } while (nullRounds < 3);
+      } while (phase2 == "loop" && nullRounds < 3);
 
       phase2 = "result";
+      phase3 = "";
     }
 
-    while (phase2 == "result")
+    while (phase2 == "result") //==> Battle Result
     {
       Console.WriteLine("todo: Result Screen");
       Console.WriteLine("Press Key to Continue");
+
       Console.ReadLine();
       Console.Clear();
+
       phase2 = "end";
+      phase3 = "";
     }
 
-    while (phase2 == "end")
+    while (phase2 == "end") //==> End Battle
     {
       Console.WriteLine("todo: End Battle Screen");
       Console.WriteLine("Press Key to Continue");
+
       Console.ReadLine();
       Console.Clear();
 
       phase1 = "gameEnd";
       phase2 = "";
+      phase3 = "";
     }
   }
 
-  // => Utility: Phase Selector
-  while (phase1 == "")
+  //todo Level Up
+
+  while (phase1 == "") //=> Phase Selector
   {
+    if (!gameRunning)
+      break;
     if (debug) phase1 = "battle";
     phase2 = "";
+    phase3 = "";
   }
 }
-
-/* Sample
-Monster was damaged and lost 1 health and now has 9 health.
-Hero was damaged and lost 1 health and now has 9 health.
-Monster was damaged and lost 7 health and now has 2 health.
-Hero was damaged and lost 6 health and now has 3 health.
-Monster was damaged and lost 9 health and now has -7 health.
-Hero wins! */
