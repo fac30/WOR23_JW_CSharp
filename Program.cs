2fda9ce9877407fc826c53e7ddaf3a0933e14234
Jason Warren <contact.jwarren@gmail.com>
2024-11-07 12:35:21 +0000

    FizzBuzz

--- Program.cs
+++ Program.cs
@@ -1,56 +1,45 @@
-﻿// SKU = Stock Keeping Unit. 
-// SKU value format: <product #>-<2-letter color code>-<size code>
-string sku = "01-MN-L";
+﻿// Output values from 1 to 100, one number per line, inside the code block of an iteration statement.
+// When the current value is divisible by 3, print the term Fizz next to the number.
+// When the current value is divisible by 5, print the term Buzz next to the number.
+// When the current value is divisible by both 3 and 5, print the term FizzBuzz next to the number.
 
-string[] product = sku.Split('-');
+/* -------- */
 
-string type = "";
-string color = "";
-string size = "";
-
-switch (product[0])
+for (int i = 1; i < 100; i++)
 {
-  case "01":
-    type = "Sweat shirt";
-    break;
-  case "02":
-    type = "T-Shirt";
-    break;
-  case "03":
-    type = "Sweat pants";
-    break;
-  default:
-    type = "Other";
-    break;
+  string output = $"{i.ToString()} • ";
+
+  if (i % 3 == 0 && i % 5 == 0)
+    output += " • FizzBuzz";
+  else if (i % 5 == 0)
+    output += " • Buzz";
+  else if (i % 3 == 0)
+    output += " • Fizz";
+  
+  Console.WriteLine(output);
 }
 
-switch (product[1])
-{
-  case "BL":
-    color = "Black";
-    break;
-  case "MN":
-    color = "Maroon";
-    break;
-  default:
-    color = "White";
-    break;
-}
+/* -------- */
 
-switch (product[2])
-{
-  case "S":
-    size = "Small";
-    break;
-  case "M":
-    size = "Medium";
-    break;
-  case "L":
-    size = "Large";
-    break;
-  default:
-    size = "One Size Fits All";
-    break;
-}
-
-Console.WriteLine($"Product: {size} {color} {type}");
+// 1
+// 2
+// 3 - Fizz
+// 4
+// 5 - Buzz
+// 6 - Fizz
+// 7
+// 8
+// 9 - Fizz
+// 10 - Buzz
+// 11
+// 12 - Fizz
+// 13
+// 14
+// 15 - FizzBuzz
+// 16
+// 17
+// 18 - Fizz
+// 19
+// 20 - Buzz
+// 21 - Fizz
+// 22
