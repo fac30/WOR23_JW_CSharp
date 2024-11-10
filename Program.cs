631660c7cd245c47cf64476248d737d0904b35f4
Jason Warren <contact.jwarren@gmail.com>
2024-11-10 18:36:01 +0000

    Pet App 0.0.1a - Delete blank default assignments to strings

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿string animalSpecies = "";
-string animalID = "";
-string animalAge = "";
-string animalPhysicalDescription = "";
-string animalPersonalityDescription = "";
-string animalNickname = "";
+﻿// string animalSpecies = "";
+// string animalID = "";
+// string animalAge = "";
+// string animalPhysicalDescription = "";
+// string animalPersonalityDescription = "";
+// string animalNickname = "";
+
+string animalSpecies;
+string animalID;
+string animalAge;
+string animalPhysicalDescription;
+string animalPersonalityDescription;
+string animalNickname;
 
 int maxPets = 8;
 string? readResult;
