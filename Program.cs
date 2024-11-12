973eac3af7cc7ed2ab9e27d9347144c86eecd8d0
Jason Warren <contact.jwarren@gmail.com>
2024-11-12 10:34:01 +0000

    Module 5 shit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,38 @@
-﻿using System.ComponentModel;
+﻿//=> Methods
 
-InitializationEventAttribute
+void SayHello()                       //?? Signature 
+{                                     //?? Definition
+  Console.WriteLine("Hello World!");  //??     |     
+}                                     //??     |     
+
+//?? This works 
+
+/***/ SayHello();
+/***/
+/***/ void SayHello()
+/***/ {
+/***/   Console.WriteLine("Hello World!");
+/***/ }
+
+//?? Often defined at end of program 
+
+/***/ int[] a = {1,2,3,4,5};
+/***/ 
+/***/ Console.WriteLine("Contents of Array:");
+/***/ PrintArray();
+/***/ 
+/***/ void PrintArray()
+/***/ {
+/***/   foreach (int x in a)
+/***/   {
+/***/     Console.Write($"{x} ");
+/***/   }
+/***/   Console.WriteLine();
+/***/ }
+
+/* Best Practises
+- Method names should be Pascal case and generally shouldn't start with digits.
+- Names for parameters should describe what kind of information the parameter represents.*/
+
+void ShowData(string a, int b, int c);
+void DisplayDate(string month, int day, int year);
